ed2bbfae3c1320acf240d60efdaf53fccb62411d
AlexKel3k <161033406+AlexKel3k@users.noreply.github.com>
2024-10-07 20:41:19 +0700

    .

---
+++ my-next/app/dashboard/page.js
@@ -0,0 +1,7 @@
+export default function dashboard(){
+    return(
+        <div>
+            <h1>This is dashboard Page</h1>
+            </div>
+    );
+}
---
+++ my-next/app/dashboard/setting/page.js
@@ -0,0 +1,7 @@
+export default function dashboard(){
+    return(
+        <div>
+            <h1>This is Setting Page</h1>
+            </div>
+    );
+}
